92a17073abc5dcbc3b788a9118f7c634ce3c9386
Starcoffeman <deniszamal@mail.ru>
2025-01-17 00:06:50 +0300

    21-40

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,210 +1,181 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        // Задача 1: Четное или нечетное число
-        System.out.println("Введите целое число:");
-        int number = scanner.nextInt();
-        if (number % 2 == 0) {
-            System.out.println("Четное");
-        } else {
-            System.out.println("Нечетное");
+    }
+
+    // Задача 1
+    public static int findMax(int a, int b) throws Exception {
+        if (a == b) {
+            throw new Exception("Числа равны");
         }
+        return Math.max(a, b);
+    }
 
-        System.out.println("Введите три целых числа:");
-        int firstNumber = scanner.nextInt();
-        int secondNumber = scanner.nextInt();
-        int thirdNumber = scanner.nextInt();
-        int minimum = Math.min(firstNumber, Math.min(secondNumber, thirdNumber));
-        System.out.println("Минимальное: " + minimum);
-
-        // Задача 3: Таблица умножения на 5
-        System.out.println("Таблица умножения на 5 :");
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("5 x " + i + " = " + (5 * i));
+    // Задача 2: Калькулятор деления
+    public static double divideNumbers(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Деление на ноль недопустимо");
         }
+        return a / b;
+    }
 
-        // Задача 4: Сумма чисел от 1 до N
-        System.out.println("Введите число N :");
-        int n = scanner.nextInt();
-        int sum = n * (n + 1) / 2;
-        System.out.println("Сумма от 1 до N: " + sum);
-
-        // Задача 5: Число Фибоначчи
-        System.out.println("Введите число N :");
-        n = scanner.nextInt();
-        System.out.println("Первые N чисел Фибоначчи:");
-        int firstFibonacci = 0;
-        int secondFibonacci = 1;
-        for (int i = 0; i < n; i++) {
-            System.out.print(firstFibonacci + " ");
-            int nextFibonacci = firstFibonacci + secondFibonacci;
-            firstFibonacci = secondFibonacci;
-            secondFibonacci = nextFibonacci;
-        }
-        System.out.println();
-
-        // Задача 6: Проверка простого числа
-        System.out.println("Введите число:");
-        number = scanner.nextInt();
-        boolean isPrime = number > 1;
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) {
-            System.out.println("Простое");
-        } else {
-            System.out.println("Не простое");
-        }
-
-        // Задача 7: Обратный порядок чисел
-        System.out.println("Введите число N:");
-        n = scanner.nextInt();
-        for (int i = n; i >= 1; i--) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        // Задача 8: Сумма четных чисел
-        System.out.println("Введите два числа A и B :");
-        int firstInput = scanner.nextInt();
-        int secondInput = scanner.nextInt();
-        sum = 0;
-        for (int i = Math.min(firstInput, secondInput); i <= Math.max(firstInput, secondInput); i++) {
-            if (i % 2 == 0) {
-                sum += i;
-            }
-        }
-        System.out.println("Сумма четных чисел: " + sum);
-
-        // Задача 9: Реверс строки
-        System.out.println("Введите строку :");
-        scanner.nextLine();
-        String input = scanner.nextLine();
-        String reversedInput = new StringBuilder(input).reverse().toString();
-        System.out.println(reversedInput);
-
-        // Задача 10: Количество цифр в числе
-        System.out.println("Введите число :");
-        number = Math.abs(scanner.nextInt());
-        int digitCount = String.valueOf(number).length();
-        System.out.println("Количество цифр: " + digitCount);
-
-        // Задача 11: Факториал числа
-        System.out.println("Введите число N :");
-        n = scanner.nextInt();
-        long factorial = 1;
-        for (int i = 2; i <= n; i++) {
-            factorial *= i;
-        }
-        System.out.println("Факториал: " + factorial);
-
-        // Задача 12: Сумма цифр числа
-        System.out.println("Введите число :");
-        number = Math.abs(scanner.nextInt());
-        sum = 0;
-        while (number > 0) {
-            sum += number % 10;
-            number /= 10;
-        }
-        System.out.println("Сумма цифр: " + sum);
-
-        // Задача 13: Палиндром
-        System.out.println("Введите строку :");
-        input = scanner.nextLine();
-        reversedInput = new StringBuilder(input).reverse().toString();
-        if (input.equals(reversedInput)) {
-            System.out.println("Палиндром");
-        } else {
-            System.out.println("Не палиндром");
-        }
-
-        // Задача 14: Найти максимальное число в массиве
-        System.out.println("Введите размер массива:");
-        n = scanner.nextInt();
-        int[] array = new int[n];
-        System.out.println("Введите элементы массива:");
-        for (int i = 0; i < n; i++) {
-            array[i] = scanner.nextInt();
-        }
-        int maximum = Arrays.stream(array).max().getAsInt();
-        System.out.println("Максимальное число: " + maximum);
-
-        // Задача 15: Сумма всех элементов массива
-        int totalSum = Arrays.stream(array).sum();
-        System.out.println("Сумма элементов массива: " + totalSum);
-
-        // Задача 16: Количество положительных и отрицательных чисел
-        long positiveCount = Arrays.stream(array).filter(x -> x > 0).count();
-        long negativeCount = Arrays.stream(array).filter(x -> x < 0).count();
-        System.out.println("Положительных: " + positiveCount + ", Отрицательных: " + negativeCount);
-
-        // Задача 17: Простые числа в диапазоне
-        System.out.println("Введите два числа A и B :");
-        firstInput = scanner.nextInt();
-        secondInput = scanner.nextInt();
-        System.out.println("Простые числа в диапазоне:");
-        for (int i = Math.min(firstInput, secondInput); i <= Math.max(firstInput, secondInput); i++) {
-            if (i > 1 && isPrime(i)) {
-                System.out.print(i + " ");
-            }
-        }
-        System.out.println();
-
-        // Задача 18: Подсчет гласных и согласных
-        System.out.println("Введите строку :");
-        input = scanner.nextLine().toLowerCase();
-        int vowelCount = 0;
-        int consonantCount = 0;
-        for (char character : input.toCharArray()) {
-            if ("aeiouаеёиоуыэюя".indexOf(character) >= 0) {
-                vowelCount++;
-            } else if (Character.isLetter(character)) {
-                consonantCount++;
-            }
-        }
-        System.out.println("Гласных: " + vowelCount + ", Согласных: " + consonantCount);
-
-        // Задача 19: Перестановка слов в строке
-        System.out.println("Введите строку :");
-        input = scanner.nextLine();
-        String[] words = input.split(" ");
-        Collections.reverse(Arrays.asList(words));
-        System.out.println(String.join(" ", words));
-
-        // Задача 20: Число Армстронга
-        System.out.println("Введите число :");
-        number = scanner.nextInt();
-        int temporaryNumber = number;
-        int numberOfDigits = String.valueOf(number).length();
-        int armstrongSum = 0;
-        while (temporaryNumber > 0) {
-            armstrongSum += Math.pow(temporaryNumber % 10, numberOfDigits);
-            temporaryNumber /= 10;
-        }
-        if (number == armstrongSum) {
-            System.out.println("Число Армстронга");
-        } else {
-            System.out.println("Не число Армстронга");
+    // Задача 3: Конвертация строки в число
+    public static int convertStringToNumber(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Некорректное число");
         }
     }
 
-    private static boolean isPrime(int number) {
-        if (number <= 1) {
-            return false;
+    // Задача 4: Проверка возраста
+    public static void checkAge(int age) {
+        if (age < 0 || age > 150) {
+            throw new IllegalArgumentException("Некорректный возраст");
         }
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) {
-                return false;
+    }
+
+    // Задача 5: Нахождение квадратного корня
+    public static double findSquareRoot(double number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Отрицательное число");
+        }
+        return Math.sqrt(number);
+    }
+
+    // Задача 6: Вычисление факториала
+    public static long factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Число должно быть неотрицательным");
+        }
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    // Задача 7: Проверка массива на нули
+    public static void checkArrayForZeros(int[] array) {
+        for (int number : array) {
+            if (number == 0) {
+                throw new IllegalArgumentException("Массив содержит нули");
             }
         }
-        return true;
+    }
+
+    // Задача 8: Возведение числа в степень
+    public static double power(double base, int exponent) {
+        if (exponent < 0) {
+            throw new IllegalArgumentException("Степень не может быть отрицательной");
+        }
+        return Math.pow(base, exponent);
+    }
+
+    // Задача 9: Обрезка строки
+    public static String truncateString(String str, int length) {
+        if (length > str.length()) {
+            throw new IllegalArgumentException("Длина превышает длину строки");
+        }
+        return str.substring(0, length);
+    }
+
+    // Задача 10: Поиск элемента в массиве
+    public static int findElementInArray(int[] array, int element) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == element) {
+                return i;
+            }
+        }
+        throw new NoSuchElementException("Элемент не найден");
+    }
+
+    // Задача 11: Конвертация числа в двоичную систему
+    public static String toBinary(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Число должно быть положительным");
+        }
+        return Integer.toBinaryString(number);
+    }
+
+    // Задача 12: Проверка делимости
+    public static boolean isDivisible(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Деление на ноль недопустимо");
+        }
+        return a % b == 0;
+    }
+
+    // Задача 13: Чтение элемента списка
+    public static String getElementFromList(List<String> list, int index) {
+        if (index < 0 || index >= list.size()) {
+            throw new IndexOutOfBoundsException("Индекс вне границ");
+        }
+        return list.get(index);
+    }
+
+    // Задача 14: Проверка сложности пароля
+    public static void checkPasswordStrength(String password) {
+        if (password == null || password.length() < 8) {
+            throw new IllegalArgumentException("Слабый пароль");
+        }
+    }
+
+    // Задача 15: Проверка корректности даты
+    public static void validateDate(String date) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            LocalDate.parse(date, formatter);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Неверный формат даты");
+        }
+    }
+
+    // Задача 16: Конкатенация строк
+    public static String concatenateStrings(String str1, String str2) {
+        if (str1 == null || str2 == null) {
+            throw new NullPointerException("Одна из строк равна null");
+        }
+        return str1 + str2;
+    }
+
+    // Задача 17: Остаток от деления
+    public static int getRemainder(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Деление на ноль недопустимо");
+        }
+        return a % b;
+    }
+
+    // Задача 18: Квадратный корень
+    public static double calculateSquareRoot(double number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Отрицательное число");
+        }
+        return Math.sqrt(number);
+    }
+
+    // Задача 19: Конвертация температуры
+    public static double convertTemperature(double celsius) {
+        final double ABSOLUTE_ZERO = -273.15;
+        if (celsius < ABSOLUTE_ZERO) {
+            throw new IllegalArgumentException("Температура ниже абсолютного нуля");
+        }
+        return celsius * 9 / 5 + 32;
+    }
+
+    // Задача 20: Проверка строки на пустоту
+    public static void checkStringNotEmpty(String str) {
+        if (str == null || str.isEmpty()) {
+            throw new IllegalArgumentException("Строка пуста или равна null");
+        }
     }
 }
+
